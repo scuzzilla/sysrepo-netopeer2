cebb1767425d1121e3eb7396fbd26a6b017751af
Salvatore Cuzzilla <salvatore.cuzzilla@gmail.com>
2021-02-05 15:50:55 +0100

    removing unwanted timeout options ...

--- tools/nc_schemas_extract/nc_schemas_extract.sh
+++ tools/nc_schemas_extract/nc_schemas_extract.sh
@@ -110,8 +110,8 @@ gen_capabilities_yangs() {
     local port="$(echo "${line}" | awk -F "," '{print $2}')"
 
     echo -e "${host}"
-    local cap_list=$("${nc}" --host "${host}" --port "${port}" -u "${username}" -p "${password}" \ 
-                     --timeout 30 --reply-timeout 30 --hello | awk -F "module=" '{print $2}' | awk -F "&amp" '{print $1}' 2> /dev/null)
+    local cap_list=$("${nc}" --host "${host}" --port "${port}" -u "${username}" -p "${password}" --hello | \
+                     awk -F "module=" '{print $2}' | awk -F "&amp" '{print $1}' 2> /dev/null)
 #    local cap_deviations=$("${nc}" --host "${host}" --port "${port}" -u "${username}" -p "${password}" \
 #                           --timeout 30 --reply-timeout 30 --hello | awk -F "deviations=" '{print $2}' | awk -F "<" '{print $1}' 2> /dev/null)
     
@@ -123,7 +123,7 @@ gen_capabilities_yangs() {
     do
       echo -e "YANG - Extracting ${yang} from ${host} ..."
       $("${nc}" --host "${host}" --port "${port}" -u "${username}" -p "${password}" \
-        --timeout 30 --reply-timeout 30 --get-schema "${yang}" > "${work_dir}/capabilities_yangs/${host}/${yang}.yang")
+        --get-schema "${yang}" > "${work_dir}/capabilities_yangs/${host}/${yang}.yang")
     done
     
 #    for yang in $cap_deviations
